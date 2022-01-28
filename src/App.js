93dac07dda99922ca0956c42604f6b6f70742277
Mukendi Mputu <mukendi_mputu@outlook.com>
2022-01-28 22:07:38 +0100

    sets up hero page

--- src/App.js
+++ src/App.js
@@ -3,22 +3,25 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <header>
+          <h2><a href="#">Mukendi Mputu</a></h2>
+          <nav>
+            <li><a href="#">CV</a></li>
+            <li><a href="#">Projects</a></li>
+            <li><a href="#">Contact</a></li>
+          </nav>
+        </header>
+
+        <section className="hero">
+          <div className="background-image" style={{backgroundImage: "url(assets/img/mountains.jpg)"}}></div>
+          <div className="hero-content-area">
+            <h1>Hello, I'm Mukendi</h1>
+            <h3>Bachelor of Computer Science</h3>
+            <a href="#" className="btn">Contact Me</a>
+          </div>
+        </section>
+      </div>
   );
 }
 
---
+++ src/Project.js
@@ -0,0 +1,12 @@
+
+
+const project = {
+    title: "",
+    language: ["PHP", "CSS"],
+    developer: {
+        name: "Mukendi Mputu",
+        email: "mukendi.mputu@tu-dortmund.de"
+    }
+}
+
+export default project;
